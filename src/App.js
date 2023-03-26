32a7e1e340b1cd2d23e9da29bc70da3f622639cb
romankt21 <romankt21@gmail.com>
2023-03-26 22:48:32 +0300

    ch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-
+import {useForm} from "react-hook-form";
 
 function App() {
+
+           let{register, handleSubmit} = useForm();
+    const onSubmit = (data)=> {
+        console.log(data);
+
+        }
     return (
-      <div>
+        <div>
 
 
-      </div>
+            <form onSubmit={handleSubmit (onSubmit)}>
+                <input {...register("title",{required: true})}/>
+                <input {...register("body")}/>
+
+                <input type={"submit"} value={'save post'}/>
+            </form>
+
+
+        </div>
 
 
     )
 
 
-  
 }
 
 export default App;
