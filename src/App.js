a595ba015562439fc9e8f58eb6617ac86a712c03
romankt21 <romankt21@gmail.com>
2023-03-26 18:47:34 +0300

    ch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import {createRef} from "react";
 
 
 function App() {
+
+    let login = createRef();
+    let password = createRef();
+    function onSubmit(e) {
+        console.log(login.current.value);
+        console.log(password.current.value);
+        e.preventDefault()
+    }
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type={"text"} name={'login'} ref={login}/>
+                <input type={"text"} name={'password'} ref={password}/>
+                <button>log in</button>
+
+
+
+            </form>
+
+
+        </div>
+    )
 
   
 }
