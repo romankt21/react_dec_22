49a4e89243557c9dbf751387fc019ddd6a35ef2b
romankt21 <romankt21@gmail.com>
2023-03-26 16:14:35 +0300

    ch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
-import logo from './logo.svg';
-import SimpsonComponent from "./components/SimpsonComponent";
+import {useState} from "react";
 
 function App() {
-    return (
-        <div className="wrap">
-<SimpsonComponent itemName={'bart'} pict={'https://static.simpsonswiki.com/images/thumb/6/65/Bart_Simpson.png/200px-Bart_Simpson.png'}/>
-<SimpsonComponent itemName={'homer'} pict={'https://www.kindpng.com/picc/m/364-3640965_homer-simpson-png-transparent-png.png'}/>
-<SimpsonComponent itemName={'marge'} pict={'https://w7.pngwing.com/pngs/176/337/png-transparent-bart-simpson-marge-simpson-homer-simpson-adidas-yeezy-simpson-family-simpsons-heroes-vertebrate-illustrator.png'}/>
-        </div>
-    )
-  
+    // let [login, setLogin] = useState('default');
+    // let [password, setPassword] = useState('');
+    //
+    //
+    // function onSubmit(e) {
+    //     console.log(e.target.login.value);
+    //     console.log(e.target.password.value);
+    //     e.preventDefault();
+    // }
+    //
+    // function onLoginChange (e) {
+    //     setLogin(e.target.value);
+    // }
+    //
+    // function onPasswordChange (e) {
+    //     setPassword(e.target.value);
+    // }
+
+
+
+    let [formState, setFormState] = useState({login: '', password: ''});
+    function onSubmit(e) {
+        console.log(e.target.login.value);
+        console.log(e.target.password.value);
+        e.preventDefault();
+        console.log(formState);
+    }
+    function onChange(e) {
+
+        setFormState({...formState, [e.target.name]: e.target.value});
+
+    }
+
+    return(
+    <div>
+        <form onSubmit={onSubmit}>
+
+            {/*<input type={"text"} name={'login'} value={login} onChange={onLoginChange}/>*/}
+            {/*<input type={"text"} name={'password'} value={password} onChange={onPasswordChange}/>*/}
+
+           <input type={"text"} name={'login'} value={formState.login} onChange={onChange}/>
+            <input type={"text"} name={'password'} value={formState.password} onChange={onChange}/>
+
+            <button>submit</button>
+
+
+
+
+
+
+        </form>
+
+    </div>
+    );
 }
 
 export default App;
